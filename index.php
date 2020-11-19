a3d07401653f90743025d36b830adc397193613d
ChakritDisakun63070035 <zix.zak@hotmail.com>
2020-11-20 01:13:13 +0700

    Working

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <meta charset="UTF-8"
+        <title>HELLO CODER</title>
+    </head>
+    <body>
+        <h1>name surname nickname</h1>
+        <p>Chakrit  Disakul  Izaac</p>
+        <img width="300" src="dog.jpg">
+        <img width="300" src="funny-cat.jpg">
+        <a href="https://imgur.com/gallery/JuRHg70">I LOVE THIS LINK</a>
+
+        <h2>FUNNY STORY</h2>
+        <p>ณ ร้านสะดวกซื้อแห่งหนึ่ง</p>
+        <p>ลูกค้า : คุณครับ มีผ้าอนามัยขายมั้ยครับ</p>
+        <p>คนขาย : มีค่ะ จะซื้อไปให้แฟนใช้เหรอคะ</p>
+        <p>ลูกค้า : ป่าวครับ ผมจะซื้อใส่เองนี่แหละครับ</p>
+        <p>คนขาย : ทำไมอ่ะคะ</p>
+        <p>ลูกค้า : พอดี ผมเป็นคนนอนไม่ค่อยหลับอ่ะครับ แล้วเห็นเค้าบอกว่า ถ้าใส่แล้ว จะช่วยให้หลับสนิทตลอดคืน</p>
+        <p>คนขาย : !!!!!!!!!</p>
+    </body>
+</html>
