0404a1a08ed4faf9b5523d5ec49dde492ae0669b
ChakritDisakun63070035 <73680143+ChakritDisakun63070035@users.noreply.github.com>
2020-11-20 17:00:43 +0700

    Update index.php

--- Lab13/index.php
+++ Lab13/index.php
@@ -6,13 +6,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
     <div class="container">
     <table class="table">
     <table border="1" style="width: 100%;">
         <tr>
-           <tr class="table-primary">
+           <table class="table table-dark table-hover">
            <th>Name</th>
            <th>Comment</th>
            <th>Action</th>
@@ -22,7 +26,7 @@
     while($Result = mysqli_fetch_array($res)){
     ?>
         <tr>
-            <tr class="table-danger">
+            <table class="table table-dark table-hover">
             <td><?=$Result['Name'];?></td>
             <td><?=$Result['Comment'];?></td>
             <td> <a href="delete.php?id=<?=$Result['ID'];?>"><button style=“backgroud-color: red;”>ลบ</button></a>
