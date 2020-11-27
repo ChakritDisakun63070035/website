0c9ae43aa1db5a4d5be5b8e74cafd9c75d46b3a9
ChakritDisakun63070035 <73680143+ChakritDisakun63070035@users.noreply.github.com>
2020-11-27 12:52:03 +0700

    Add files via upload

---
+++ testmbi/insert.php
@@ -0,0 +1,20 @@
+<?php
+
+include "config.inc.php";
+
+$name = $_POST['name'];
+$height = $_POST['height'];
+$weight = $_POST['weight'];
+$sum = $_POST[$height/$weight];
+
+$sql = "INSERT INTO testbmi (name , height , weight , bmi) VALUES ('$name', '$height', '$weight' , '$sum')";
+
+
+if (mysqli_query($conn, $sql)) {
+  header('location: index.php');
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+  
+mysqli_close($conn);
+?>
