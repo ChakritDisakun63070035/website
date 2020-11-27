55c0579ab6d55aab8f1116acce724ebb737034ea
ChakritDisakun63070035 <73680143+ChakritDisakun63070035@users.noreply.github.com>
2020-11-27 19:05:21 +0700

    Update insert.php

--- testmbi/insert.php
+++ testmbi/insert.php
@@ -5,7 +5,7 @@ include "config.inc.php";
 $name = $_POST['name'];
 $height = $_POST['height'];
 $weight = $_POST['weight'];
-$sum = [$weight/($height/100)**2];
+$sum = $weight/($height/100)**2;
 
 $sql = "INSERT INTO testbmi (name , height , weight , bmi) VALUES ('$name', '$height', '$weight' , '$sum')";
 
