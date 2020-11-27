520ec7220250366a36383c40c6c9ccbfce175cf7
ChakritDisakun63070035 <73680143+ChakritDisakun63070035@users.noreply.github.com>
2020-11-27 12:45:08 +0700

    Add files via upload

---
+++ testmbi/form.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+      <style>
+          body {
+              background-color: #343a40;
+              color: #FFFFFF;
+               }   
+     </style>
+<center>
+<form action = "insert.php" method = "post" id="CommentForm" >
+    name:<br>
+    <input type="text" name = "name" id="idname" placeholder="Enter Name" autocomplete="off"> <br>
+    height:<br>
+    <input type="int" name = "height" id="idheight" placeholder="Enter Height" autocomplete="off"></textarea><br> 
+    weight:<br>
+    <input type="int" name = "weight" id="idweight" placeholder="Enter Weight" autocomplete="off"></textarea><br>  
+    <input type="submit" id="weightBtn">
+  </form>
+<center>
+</body>
+</html>
